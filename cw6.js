2f85a1b5de7f4d36c9127085c466f0bb7c83f941
Danahq8 <alomrandanah@gmail.com>
2022-10-26 20:21:02 +0300

    done

---
+++ cw6.js
@@ -0,0 +1,21 @@
+alert(`مرجبا بك في موقعي اللذي سيحسب درجتك`)
+let grade= prompt("اكتب درجتك بالأرقام")
+console.log ("اكتب اسم المتغير هنا ")
+if(grade>=90){console.log("لقد حصلت على امتياز")
+
+}
+else if(grade>=80){
+   console.log ("لقد  حصلت على جيد جدا")
+}
+else if(grade>=70){
+   console.log ("لقد حصلت على جيد")
+}
+else if(grade>=60){
+    console.log("لقد حصلت على مقبول")
+}
+else if(grade>=50){
+    console.log("لقد حصت على مقبول ")
+}
+else if(grade<50){
+   console.log ("راسب")
+}
